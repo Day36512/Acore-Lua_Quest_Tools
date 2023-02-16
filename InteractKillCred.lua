37463d2c09867d43b8ee1e9c782be1aa1e32fc3d
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-02-16 15:53:06 -0700

    Update and rename SWVillager.lua to InteractKillCred.lua

--- InteractKillCred.lua
+++ InteractKillCred.lua
@@ -1,10 +1,10 @@
 --Credits to Dinkledork.
---I created this script to be used in a quest to calm down some frantic stormwind citizens.
+--I created this script to be used in a quest to calm down some frantic stormwind citizens. If player has specified item, can interact with specified npc and select the gossip item that gives the player kill credit.
 
-local npcid = 400028
-local gossipText = "Get the Stormwind Citizen to calm down..."
-local itemId = 60083 -- Soothing spices as a test, required to interact with the npc. Can set to a given quest item.
-local spellId = 139
+local npcid = 400028 --npc required for quest. Must have a gossip menu flag and be friendly
+local gossipText = "Get the Stormwind Citizen to calm down..." --Gossip dialogue. Change to Whatever
+local itemId = 60083 --Required item to interact with the npc. Can set to a given quest item. Item will be consumed on use
+local spellId = 139 --Visual effect spell to be cast on npc
 
 -- This function is called when the player first interacts with the NPC
 local function OnGossipHello(event, player, creature)
@@ -16,8 +16,8 @@ player:GossipMenuAddItem(0, gossipText, 0, 1)
 player:GossipSendMenu(1, creature)
 else
 -- Send an error message to the player if they don't have the necessary item
-player:SendBroadcastMessage("You do not have the necessary item to calm the citizen.")
-end
+player:SendBroadcastMessage("You do not have the necessary item to calm the citizen.") --can change error message to whatever you want
+end 
 end
 
 -- This function is called when the player selects an option from the gossip menu
@@ -35,11 +35,11 @@ creature:CastSpell(creature, spellId, true)
 -- Choose a random dialogue for the NPC to say after interacting
 local randomDialogue = math.random(1, 3)
 if randomDialogue == 1 then
-creature:SendUnitSay("Oh, thank you for helping me come to my senses...I should be going now...", 0)
+creature:SendUnitSay("Oh, thank you for helping me come to my senses...I should be going now...", 0) --dialogue 1
 elseif randomDialogue == 2 then
-creature:SendUnitSay("Thank you so much! I feel much better now...", 0)
+creature:SendUnitSay("Thank you so much! I feel much better now...", 0) --dialogue 2
 else
-creature:SendUnitSay("I can finally think clearly again, thank you!", 0)
+creature:SendUnitSay("I can finally think clearly again, thank you!", 0) --dialogue 3
 end
 -- Close the gossip menu
 player:GossipComplete()
