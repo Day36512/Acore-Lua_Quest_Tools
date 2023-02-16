1e132219c3479290e4e232568052f8cf3c44901c
Dinkledork <118951051+Day36512@users.noreply.github.com>
2023-02-16 15:49:16 -0700

    Update and rename SWGKillCred.lua to DirectSpellKillCred.lua

--- DirectSpellKillCred.lua
+++ DirectSpellKillCred.lua
@@ -1,5 +1,5 @@
-local SWG_IDS = {68, 400013, 400033, 400014, 1976}
-local SPELL_ID = 100141
+local SWG_IDS = {68, 400013, 400033, 400014, 1976} --NPCs you want the spell to be castable on
+local SPELL_ID = 100141 --SpellID for quest spell needed to be cast on npcs
 
 function OnSpellCast(event, caster, spell)
     local target = spell:GetTarget()
@@ -12,11 +12,11 @@ function OnSpellCast(event, caster, spell)
             end
         end
         if not isValidTarget then
-            caster:SendBroadcastMessage("Invalid target for this spell.")
+            caster:SendBroadcastMessage("Invalid target for this spell.") --Comes back invalid if target is not part of the SWG_IDS
             spell:Cancel()
         else
             -- give kill credit to NPC ID 68
-            caster:KilledMonsterCredit(68)
+            caster:KilledMonsterCredit(68) --SWG_IDS all give kill credit for this npcid, meaning you don't have to modify killcredit tables in sql
         end
     end
 end
